b23ed450f579ab7af292ac685a58357c5559bd2c
kty0529 <kty0529@gmail.com>
2019-11-03 12:50:47 +0900

    1.0.0 최종

---
+++ images/core.footer.js
@@ -0,0 +1,30 @@
+var grid = document.querySelector('#list-container .grid');
+
+if ( grid !== null ) {
+	var msnry = new Masonry( grid, {
+		initLayout: false,
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		horizontalOrder: true
+	});
+
+	imagesLoaded( grid ).on( 'done', function() {
+		msnry.layout();
+		grid.parentNode.classList.add('loaded');
+	});
+}
+
+
+var toggleSidebarBtn = document.querySelector('#header .toggle-sidebar');
+var sidebar          = document.querySelector('#sidebar');
+var overlay          = document.querySelector('#overlay');
+
+toggleSidebarBtn.addEventListener('click', function() {
+	var root = document.querySelector('html');
+	root.classList.add('sidebar-active', 'scroll-lock');
+});
+
+overlay.addEventListener('click', function() {
+	var root = document.querySelector('html');
+	root.classList.remove('sidebar-active', 'scroll-lock');
+});
